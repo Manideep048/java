64873e9a6742412a28b87595f69989bc86cb0711
Manideep_048 <100527457+Manideep048@users.noreply.github.com>
2022-09-20 15:19:24 +0530

    Update Arithmetic.java

--- Arithmetic.java
+++ Arithmetic.java
@@ -1,26 +1,69 @@
 import java.io.*;
-import java.util.*;
-import java.text.*;
-import java.math.*;
-import java.util.regex.*;
+import java.util.Scanner;
 class Arithmetic
 {
-    Arithmetic(){
-
-    }
+	void Add(int x,int y)
+	{
+		System.out.println("Sum of "+x+ " and "+y+" is "+(x+y));	
+	}
 }
-class Adder extends Arithmetic{
-    Adder(){
-
-    }
-    int add(int a, int b){
-        return a+b;
-    }
+class Adder extends Arithmetic
+{
+	void Add(double x,double y)
+	{
+		System.out.println("Sum of "+x+ " and "+y+" is "+(x+y));
+	}
+	void Add(String x,String y)
+	{
+		System.out.println("Sum of "+x+ " and "+y+" is "+(x+y));
+	}
 }
-    class solution{
-    public static void main(String Args[]){
-    Adder a = new Adder();
-    System.out.println("MY super is:" + a.getClass().getSuperclass().getName());
-    System.out.println(a.add(10,32) + " " + a.add(10,3) + " " + a.add(10,10) + " \n ");
-    }
+class sum
+{
+	public static void main(String args[])
+	{
+		int c,k=1;
+		Scanner s = new Scanner(System.in);
+		Adder ad = new Adder();
+		while(k!=0)
+		{
+			System.out.println(" 1 : Addition of two integer");
+			System.out.println(" 2 : Addition of two decimal number");
+			System.out.println(" 3 : Concatenation of String");
+			System.out.println(" 4 : Exit");
+			System.out.print("Enter your choice:");
+			c=s.nextInt();
+			if(c==1)
+			{
+				int a,b;
+				System.out.print("Enter the first integer:");
+				 a=s.nextInt();
+				System.out.print("Enter the second integer:");
+				b=s.nextInt();
+				ad.Add(a,b);
+			}
+			else if(c==2)
+			{
+				System.out.print("Enter the first decimal integer:");
+				float m=s.nextFloat();
+				System.out.print("Enter the second decimal integer:");
+				float n=s.nextFloat();
+				ad.Add(m,n);	
+			}
+			else if(c==3)
+			{
+				System.out.print("Enter the first string:");
+				String str = s.next();
+				char k1=str.charAt(0);
+				System.out.print("Enter the second string:");
+				String str1 = s.next();
+				char l=str1.charAt(0);
+				ad.Add(k1,l);	
+			}
+			else if(c==4)
+				break;
+			else
+			System.out.println("Enter a valid choice");
+		}	
+	}
 }
